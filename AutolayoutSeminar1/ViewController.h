e35c8aa64bdd7801fd13fe3ff9ca575411b8628a
satoutakeshi <sato.takeshi1987@gmail.com>
2015-08-06 21:56:24 +0900

    Initial Commit

---
+++ AutolayoutSeminar1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutSeminar1
+//
+//  Created by satoutakeshi on 2015/08/06.
+//  Copyright (c) 2015年 satoutakeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
